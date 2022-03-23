d4e9b036841959d9efd5626c2c4359d43b4778c5
l-ram <larryramjutton@gmail.com>
2022-03-23 09:30:47 +0100

    set-up the page

---
+++ script.js
@@ -0,0 +1,18 @@
+const css = document.querySelector("h3");
+const colour1 = document.querySelector(".colour1")
+const colour2 = document.querySelector(".colour2")
+
+console.log(css);
+console.log(colour1);
+console.log(colour2);
+
+colour1.addEventListener(input, function(){
+
+
+
+})
+
+colour2.addEventListener(input, function(){
+	
+
+})
